328dafa88fef49a82443bb777d3c1b2c10d0098a
yaoshining <ysnlvlqw@msn.com>
2014-12-04 23:03:00 +0800

    First version of td-components

---
+++ td-list-view/td-list-view.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 世宁 on 14-12-4.
+ */
+(function(window,angular){
+    'use strict'
+    angular.module('td-list-view',['ng']).service('listViewService',['$log',function($log){
+        $log.info('listViewService');
+    }]);
+})(window,window.angular)
